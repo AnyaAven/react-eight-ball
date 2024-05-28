e48435b755ea43ae5176a3d40235ec1159107f10
Anya Aven <leafeon96@gmail.com>
2024-05-28 14:38:40 -0400

    EightBall handleClick logic

--- src/EightBall.jsx
+++ src/EightBall.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import { sample } from "lodash";
 
 /**
- * EightBall Component
+ * EightBall Component <Button/>
  * Gives you a random message when clicked with a color associated with it
  *
  * State:
@@ -15,7 +16,18 @@ function EightBall({ answers }){
   const [msg, setMsg] = useState("Think of a Question");
   const [color, setColor] = useState("black");
 
+  const randomMsgColor = sample(answers);
 
+  function handleClick(evt){
+    setMsg(randomMsgColor.msg)
+    setColor(randomMsgColor.color)
+  }
+
+  return (
+    <button className="EightBall" onClick={handleClick}>
+
+    </button>
+  )
 }
 
 export default EightBall;
